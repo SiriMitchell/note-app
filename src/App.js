1ce9de4eb68f28c03e6f261c9e00e1ddbfe5263b
SiriMitchell <mitchellschoolofexcellence@gmail.com>
2019-08-08 21:53:33 -0500

    step 12. working on permission errors

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 import List from './components/List';
 import Note from './components/Note';
+import axios from 'axios';
+import urlFor from './helpers/urlFor';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showNote: false
+    };
+  }
+
+toggleNote = () => {
+  this.setState({
+    showNote: ! this.state.showNote
+  });
+}
+
+getNotes = () => {
+  axios.get(urlFor('notes'))
+  .then((res) => console.log(res.data) )
+  .catch((err) => console.log(err.response.data) );
+}
+
   render() {
+    const { showNote } = this.state;
+
     return (
       <div className="App">
-        <Nav />
-        <Lists />
-        <Note />
+        <Nav toggleNote={this.toggleNote} showNote={showNote} />
+        { showNote ? <Note /> : <List getNotes={this.getNotes} /> }
       </div>
     );
   }
